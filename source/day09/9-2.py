c139c23ef5986365f5fb094a005023ccbd63e40d
Rick Mohring <rmmohring@live.com>
2023-12-09 15:17:41 -0500

    Day 9 solutions

---
+++ source/day09/9-2.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+
+fh = open("input9-1.dat")
+# fh = open("test.dat")
+
+lines = fh.readlines()
+rows = [x.strip("\n").split(" ") for x in lines]
+arrays = []
+for row in rows:
+    tmp = [int(x) for x in row]
+    arrays.append(pd.Series(tmp))
+
+shift = {}
+offset = {}
+next_val = {}
+for idx, arr in enumerate(arrays):
+    offset[idx] = 0
+    tmp = arr.copy()
+    # print("--------------")
+    # print(tmp.to_list())
+
+    sign = -1
+    for j in range(100):
+        # print(idx, j)
+        tmp = tmp.diff()
+        # print(tmp.dropna().to_list())
+        offset[idx] += tmp.dropna().to_list()[0] * sign
+        sign *= -1
+        if np.all(tmp.fillna(0) == 0):
+            shift[idx] = j
+            next_val[idx] = arr.to_list()[0] + offset[idx]
+            break
+print(next_val)
+print(sum(next_val.values()))
