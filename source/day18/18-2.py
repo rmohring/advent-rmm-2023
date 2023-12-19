3d6ed907d2bf6983905524deaccaec34c84d7e75
Rick Mohring <rmohring@princeton.com>
2023-12-18 20:19:41 -0500

    adding day18... other days on a different laptop

---
+++ source/day18/18-2.py
@@ -0,0 +1,122 @@
+import numpy as np
+import copy
+
+fh = open("input18-1.dat")
+#fh = open("test.dat")
+
+lines = fh.readlines()
+
+instructions = []
+for line in lines:
+    direction, num, color = line.strip().split()
+    color = color.replace("(","").replace(")","")
+    instructions.append( (direction, int(num), color))
+
+# Convert these
+real_instructions = []
+for junk, othajunk, code in instructions:
+    count = int(code[1:6], 16)   # Convert from hex
+    direction = code[-1]
+    real_instructions.append((direction, count))
+
+
+nav = {  
+    "3": (-1,0),
+    "1": (1,0),
+    "0": (0,1),
+    "2": (0,-1),
+}
+
+def tupleadd(a,b,mult=1):
+    return (a[0]+b[0]*mult, a[1]+b[1]*mult)
+
+from collections import Counter
+pos = (0,0)
+# cells = set()
+# cells.add(pos)
+ccc = Counter()
+cells = []
+cells.append(pos)
+for direction, count in real_instructions:
+    #for i in range(count):
+    ccc[direction] += count
+    pos = tupleadd(pos, nav[direction], mult=count)
+    # cells.add(pos)
+    cells.append(pos)
+
+
+nrows = max((x[0] for x in cells)) - min((x[0] for x in cells)) + 1
+ncols = max((x[1] for x in cells)) - min((x[1] for x in cells)) + 1
+offset_rows = min((x[0] for x in cells)) 
+offset_cols = min((x[1] for x in cells))
+
+print(nrows, ncols, offset_rows, offset_cols)
+raise
+
+grid = []
+for i in range(nrows):
+    grid.append( ["."]*ncols )
+
+for i,j in cells:
+    grid[i-offset_rows][j-offset_cols] = "#"
+
+for i in range(nrows):
+    for j in range(ncols):
+        print(grid[i][j], end="")
+    print()
+
+
+# ctr = 0
+# for i, row in enumerate(grid):
+#     if i==0:
+#         continue
+#     for j, c in enumerate(row):
+#         if j==0:
+#             continue
+#         if (filled[i][j-1] == "#") and (filled[i-1][j] == "#"):
+#             filled[i][j] = "#"
+
+# I know it's a loop, and from looking at it, I know that (1,1) is inside
+
+
+
+tmp = copy.deepcopy(grid)
+tmp[1-offset_rows][1-offset_cols] = "x"
+
+found = True
+ctr = 0
+while found:
+    ctr += 1
+    if ctr % 10==0:
+        print(ctr)
+    found = False
+    filled = copy.deepcopy(tmp)
+    for i, row in enumerate(filled):
+        if (i==0) or (i==len(filled)):
+            continue
+        #print(i, row, np.where(np.array(row)=='x'))
+        for j in np.where(np.array(row)=='x')[0]:
+            if (j==0) or (j==len(row)):
+                continue
+            if tmp[i][j-1] == ".":
+                tmp[i][j-1] = "x"
+                found = True
+            if tmp[i][j+1] == ".":
+                tmp[i][j+1] = "x"
+                found = True
+            if tmp[i-1][j] == ".":
+                tmp[i-1][j] = "x"
+                found = True
+            if tmp[i+1][j] == ".":
+                tmp[i+1][j] = "x"
+                found = True
+
+ctr = 0
+for i in range(nrows):
+    for j in range(ncols):
+        print(filled[i][j], end="")
+        ctr += (filled[i][j] in ["#","x"])
+    print()
+
+print("Answer:", ctr)
+        
